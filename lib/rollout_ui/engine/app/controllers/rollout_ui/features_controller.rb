a9ee28968252734dd4635dd23e2e3df5277e5ac6
Simon Le Parc <simon.leparc@outright.com>
2013-05-01 10:32:03 -0700

    tests passing with rollout 2.0.0c

--- lib/rollout_ui/engine/app/controllers/rollout_ui/features_controller.rb
+++ lib/rollout_ui/engine/app/controllers/rollout_ui/features_controller.rb
@@ -3,7 +3,7 @@ module RolloutUi
     before_filter :wrapper, :only => [:index, :create]
 
     def index
-      @features = @wrapper.features.map{ |feature| RolloutUi::Feature.new(feature) }
+      @features = @wrapper.features.map{ |feature| RolloutUi.rollout.get(feature) }
     end
 
     def update
